90c405186490f6665bc1e0a2dae094cc2397734c
Brendon Otto <brendon@brendonotto.com>
2016-12-04 21:57:25 -0600

    Completed Chapter 27 of Pro ASP.NET MVC 5 on WebApi

--- WebServices/Controllers/HomeController.cs
+++ WebServices/Controllers/HomeController.cs
@@ -1,45 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using WebServices.Models;
 
 namespace WebServices.Controllers
 {
     public class HomeController : Controller
     {
-        private ReservationRepository repo = ReservationRepository.Current;
-
-        // GET: Home
         public ViewResult Index()
         {
-            return View(repo.GetAll());
+            return View();
         }
 
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                repo.Add(item);
-                return RedirectToAction("Index");
-            }
-            else
-                return View("Index");
-        }
-
-        public ActionResult Remove(int id)
-        {
-            repo.Remove(id);
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Update(Reservation item)
-        {
-            if (ModelState.IsValid && repo.Update(item))
-                return RedirectToAction("Index");
-            else
-                return View("Index");
-        }
     }
 }
